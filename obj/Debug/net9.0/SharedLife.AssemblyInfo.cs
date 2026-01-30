87792125bc6eff271f25eb3f79a6491ce0fa8792
SelinaKunwar1 <kunwarselina39@gmail.com>
2026-01-30 21:06:38 +0545

    Restructured: moved files from SharedLife/SharedLife to root for cleaner structure

--- obj/Debug/net9.0/SharedLife.AssemblyInfo.cs
+++ obj/Debug/net9.0/SharedLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8fa6e6a4ef0279db7438468d503342d347a1af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770816b4483009609e904d8f4d0db45e05a01c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
